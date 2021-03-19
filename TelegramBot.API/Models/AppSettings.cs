976166de91f4952723de7ff156515cda0fa832dd
Oleg Lyamzin <o.s.like@yandex.ru>
2021-03-19 15:52:15 +0300

    add url

--- TelegramBot.API/Models/AppSettings.cs
+++ TelegramBot.API/Models/AppSettings.cs
@@ -7,7 +7,7 @@ namespace TelegramBot.API.Models
 {
     public static class AppSettings
     {
-        public static string Url { get; set; }  = "";
+        public static string Url { get; set; }  = "http://telegrambottemplate.azurewebsites.net:443/{0}";
         public static string Name { get; set; } = "Tamplate_bot";
         public static string Key { get; set; }  = "1667348802:AAG7vUsH34oxyajl0x1rygeB9UXlLynQA8k";
     }
