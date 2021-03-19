dc89d47870825462cb3eea9605fa49dbfb8a336f
Oleg Lyamzin <o.s.like@yandex.ru>
2021-03-19 14:17:00 +0300

    Init API

---
+++ TelegramBot.API/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TelegramBot.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
